5c092703808cf8c9c3a1491d6228d0b3e1e7c3ed
EshanKaria <70959196+EshanKaria@users.noreply.github.com>
2020-09-08 19:38:45 +0530

    Add files via upload

---
+++ Project1.java
@@ -0,0 +1,9 @@
+package project.pkg1;
+public class Project1 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("Hello");
+        System.out.println("My Name Is Eshan");
+    }
+}
